11f11494d1b22015bac50300ab10ec454a8c14f9
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-16 18:08:27 -0300

    added styles to div containing movie's name and description

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -30,15 +30,30 @@ export default function Banner({
         handleRandomMovie();
     }, [netflixOriginals]);
 
-
     return (
-        <div>
-            <div>
+        <div className="flex h-screen items-center">
+            <div className='absolute top-0 left-0 -z-10 h-screen w-screen'>
                 <Image
                     layout="fill"
                     src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
                     objectFit="cover"
                 />
+            </div>
+
+            <div className="flex-col items-center lg:p-20 sm:p-10">
+                <h1 className="text-2xl font-bold lg:text-7xl">
+                    {
+                        movie?.title ||
+                        movie?.original_title ||
+                        movie?.name
+                    }
+                </h1>
+                <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2lx">
+                    {
+                        movie?.overview
+                    }
+                </p>
+
             </div>
         </div>
     );
