4e5a1d06b8eba29006a958be0ce9bd0f39981d5b
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-16 13:33:43 -0300

    Rendering Header component and styles for Header related

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,28 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+//Components
+import Header from '../components/Header';
+
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Netflix_NextJs</title>
+        <title>Netflix - NextJS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
+
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Modal */}
     </div>
   )
 }
