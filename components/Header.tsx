89d6da7876df9a92567c072a98a93bbb1cc4a4df
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-16 13:32:29 -0300

    Header component created and almost finished

---
+++ components/Header.tsx
@@ -0,0 +1,49 @@
+//Icons
+import { BellIcon, SearchIcon } from '@heroicons/react/solid';
+
+//NextJs Components
+import Link from 'next/link';
+
+
+export default function Header() {
+
+
+    return (
+        <header>
+            <div className="flex items-center space-x-2 md:space-x-10">
+                <img
+                    src="https://rb.gy/ulxxee"
+                    width={100}
+                    height={100}
+                    alt="netflixLogo"
+                    className="cursor-pointer object-contain"
+                />
+
+                <ul className="hidden space-x-4 md:flex">
+                    <li className="headerLink">Home</li>
+                    <li className="headerLink">TV Shows</li>
+                    <li className="headerLink">Movies</li>
+                    <li className="headerLink">New & Popular</li>
+                    <li className="headerLink">My List</li>
+                </ul>
+            </div>
+
+
+            <div className="flex items-center space-x-4 text-sm font-light">
+                <SearchIcon className="hidden h-6 w-6 cursor-pointer sm:inline"
+                />
+                <p className="hidden cursor-pointer lg:inline">
+                    Kids
+                </p>
+                <BellIcon className="h-6 w-6 cursor-pointer" />
+                <Link href="/account">
+                    <img
+                        src="https://rb.gy/g1pwyx"
+                        alt="go to your account"
+                        className="cursor-pointer rounded"
+                    />
+                </Link>
+            </div>
+        </header>
+    );
+};
