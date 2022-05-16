a2cbf96a441509c84c77c216125af884b7568530
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-16 13:43:40 -0300

    bg

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
     }, []);
 
     return (
-        <header className={`${isScrolled && "bg-[white]"}`}>
+        <header className={`${isScrolled && "bg-[#141414]"}`}>
             <div className="flex items-center space-x-2 md:space-x-10">
                 <img
                     src="https://rb.gy/ulxxee"
