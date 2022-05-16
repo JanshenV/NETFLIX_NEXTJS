424c025cc1e9eee7feaf7de1fae5abf5b22072c1
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-16 15:02:00 -0300

    Banner component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,11 @@ import Head from 'next/head';
 
 //Components
 import Header from '../components/Header';
+import Banner from '../components/Banner';
 
 const Home: NextPage = () => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="flex-col items-center  relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Netflix - NextJS</title>
         <link rel="icon" href="/favicon.ico" />
@@ -17,7 +18,7 @@ const Home: NextPage = () => {
       <Header />
 
       <main>
-        {/* Banner */}
+        <Banner />
         <section>
           {/* Row */}
           {/* Row */}
