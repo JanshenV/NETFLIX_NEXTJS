f3a32823064c44b117978726f907fa5245679722
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-16 14:00:41 -0300

    added some styles for homePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+
+//NextJS components
+import Head from 'next/head';
 
 //Components
 import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Netflix - NextJS</title>
         <link rel="icon" href="/favicon.ico" />
